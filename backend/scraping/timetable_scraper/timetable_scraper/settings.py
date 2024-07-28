0cdafc233bf7e0d344002258275696c3d5ba0cd2
brennanh07 <brennanh07@vt.edu>
2024-07-28 16:45:54 -0400

    renamed to timetable scraper

--- backend/scraping/timetable_scraper/timetable_scraper/settings.py
+++ backend/scraping/timetable_scraper/timetable_scraper/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for section_scraper project
+# Scrapy settings for timetable_scraper project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,14 +7,14 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = "section_scraper"
+BOT_NAME = "timetable_scraper"
 
-SPIDER_MODULES = ["section_scraper.spiders"]
-NEWSPIDER_MODULE = "section_scraper.spiders"
+SPIDER_MODULES = ["timetable_scraper.spiders"]
+NEWSPIDER_MODULE = "timetable_scraper.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "section_scraper (+http://www.yourdomain.com)"
+#USER_AGENT = "timetable_scraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -45,13 +45,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    "section_scraper.middlewares.SectionScraperSpiderMiddleware": 543,
+#    "timetable_scraper.middlewares.TimetableScraperSpiderMiddleware": 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    "section_scraper.middlewares.SectionScraperDownloaderMiddleware": 543,
+#    "timetable_scraper.middlewares.TimetableScraperDownloaderMiddleware": 543,
 #}
 
 # Enable or disable extensions
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    "section_scraper.pipelines.SectionScraperPipeline": 300,
+#    "timetable_scraper.pipelines.TimetableScraperPipeline": 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
