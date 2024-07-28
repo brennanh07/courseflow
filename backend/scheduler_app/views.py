a06f9b92a98279de4b0ff1287bfc14479ddeee9d
brennanh07 <brennanh07@vt.edu>
2024-07-28 14:36:19 -0400

    created views/viewsets

--- backend/scheduler_app/views.py
+++ backend/scheduler_app/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Subject, Professor, Section, User, Preference, Weight, Schedule, ScheduleLog
+from .serializers import SubjectSerializer, ProfessorSerializer, SectionSerializer, UserSerializer, PreferenceSerializer, WeightSerializer, ScheduleSerializer, ScheduleLogSerializer
+fr
 # Create your views here.
+class SubjectViewSet(viewsets.ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+    
+class ProfessorViewSet(viewsets.ModelViewSet):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
+    
+class SectionViewSet(viewsets.ModelViewSet):
+    queryset = Section.objects.all()
+    serializer_class = SectionSerializer
+    
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class PreferenceViewSet(viewsets.ModelViewSet):
+    queryset = Preference.objects.all()
+    serializer_class = PreferenceSerializer
+    
+class WeightViewSet(viewsets.ModelViewSet):
+    queryset = Weight.objects.all()
+    serializer_class = WeightSerializer
+    
+class ScheduleViewSet(viewsets.ModelViewSet):
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
+    
+class ScheduleLogViewSet(viewsets.ModelViewSet):
+    queryset = ScheduleLog.objects.all()
+    serializer_class = ScheduleLogSerializer
+    
+
