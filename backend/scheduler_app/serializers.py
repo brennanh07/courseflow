d662fc6db049ad7ccb6815ee7da9dbc2c6d1949c
brennanh07 <brennanh07@vt.edu>
2024-07-28 14:13:17 -0400

    created serializers for models

---
+++ backend/scheduler_app/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from .models import Subject, Professor, Section, User, Preference, Weight, Schedule, ScheduleLog
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = '__all__'
+        
+class ProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Professor
+        fields = '__all__'
+        
+class SectionSerializer(serializers.ModelSerializer):
+    professor = ProfessorSerializer()
+    
+    class Meta:
+        model = Section
+        fields = '__all__'
+        
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        
+        
+class PreferenceSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    
+    class Meta:
+        model = Preference
+        fields = '__all__'
+        
+        
+class WeightSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    
+    class Meta:
+        model = Weight
+        fields = '__all__'
+        
+        
+class ScheduleSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    
+    class Meta:
+        model = Schedule
+        fields = '__all__'
+        
+        
+class ScheduleLogSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    
+    class Meta:
+        model = ScheduleLog
+        fields = '__all__'
