b65c7a2e24eb0227dc7fba5e1c535fcb8d2bd4d7
brennanh07 <brennanh07@vt.edu>
2024-08-31 00:13:24 -0400

    API configuration

--- backend.0/scheduler/urls.py
+++ backend.0/scheduler/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import DefaultRouter
 from . import views
+from scheduler.views import (
+    SubjectViewSet, ProfessorViewSet, SectionViewSet, SectionTimeViewSet, UserViewSet, PreferenceViewSet, 
+    WeightViewSet, ScheduleViewSet, ScheduleLogViewSet, GenerateScheduleView
+)
+
+
+router = DefaultRouter()
+router.register(r'subjects', SubjectViewSet)
+
 
 urlpatterns = [
-    path('', views.index, name='class_scheduler-home'),
+    path('generate-schedules/', GenerateScheduleView.as_view(), name='generate-schedules'),
 ]
--- backend.0/scheduler/views.py
+++ backend.0/scheduler/views.py
@@ -1,6 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from scheduler.models import Subject, Professor, Section, SectionTime, User, Preference, Weight, Schedule, ScheduleLog
-from scheduler.serializers import SubjectSerializer, ProfessorSerializer, SectionSerializer, SectionTimeSerializer, UserSerializer, PreferenceSerializer, WeightSerializer, ScheduleSerializer, ScheduleLogSerializer
+from scheduler.serializers import (
+    SubjectSerializer, ProfessorSerializer, SectionSerializer, SectionTimeSerializer, UserSerializer, PreferenceSerializer, 
+    WeightSerializer, ScheduleSerializer, ScheduleLogSerializer, ScheduleInputSerializer, BreakSerializer
+)
+from . import schedule_scoring
 
 class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
@@ -37,4 +43,13 @@ class ScheduleViewSet(viewsets.ModelViewSet):
 class ScheduleLogViewSet(viewsets.ModelViewSet):
     queryset = ScheduleLog.objects.all()
     serializer_class = ScheduleLogSerializer
+    
+class GenerateScheduleView(APIView):
+    def post(self, request):
+        serializer = ScheduleInputSerializer(data=request.data)
+        if serializer.is_valid():
+            user_input = serializer.validated_data
+            generated_schedules = schedule_scoring(user_input)
+            return Response({"schedules": generated_schedules}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
