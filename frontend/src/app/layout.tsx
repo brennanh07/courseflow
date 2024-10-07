b113c6fc8a3e898cc4be87dc22ef6c5140487333
brennanh07 <brennanh07@vt.edu>
2024-10-07 11:05:14 -0400

    New header

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,9 +19,42 @@ export default function RootLayout({
   return (
     <html data-theme="mytheme" lang="en">
       <body className="font-main">
-        <header className="bg-primary text-white font-main h-12 flex justify-left items-center pl-3">
-          Class Schedule Generator
-        </header>
+        <div className="navbar bg-base-100">
+          <div className="flex-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-6 ml-4"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="flex-1">
+            <a className="btn btn-ghost text-2xl text-primary">SmartClass Scheduler</a>
+          </div>
+          <div className="flex-none">
+            <button className="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-5 w-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
         {children}
         <footer className="bg-secondary text-neutral font-main h-12 flex justify-left items-center pl-3">
           Created by: Brennan Humphrey
