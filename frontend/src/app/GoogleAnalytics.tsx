4a6a0aeb5d00152022142f84ed54f16b6134b0d5
brennanh07 <brennanh07@vt.edu>
2024-10-12 15:30:43 -0400

    Google analytics implementation

---
+++ frontend/src/app/GoogleAnalytics.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import Script from "next/script";
+
+interface GoogleAnalyticsProps {
+  GA_MEASUREMENT_ID: string;
+}
+
+export default function GoogleAnalytics({
+  GA_MEASUREMENT_ID,
+}: GoogleAnalyticsProps) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString();
+    window.gtag("config", GA_MEASUREMENT_ID, {
+      page_path: url,
+    });
+  }, [pathname, searchParams, GA_MEASUREMENT_ID]);
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Metadata } from "next";
 import "./globals.css";
-import Link from "next/link";
+import SmartClassSchedulerButton from "./NavButton";
+import GoogleAnalytics from "./GoogleAnalytics";
 
 export const metadata: Metadata = {
   title: {
@@ -23,46 +24,11 @@ export default function RootLayout({
         <div className="navbar bg-base-100">
           <div className="flex-none"></div>
           <div className="flex-1">
-            <Link href="/" legacyBehavior passHref>
-              <a className="btn btn-ghost text-2xl text-primary flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="size-7 text-black"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                SmartClass Scheduler
-              </a>
-            </Link>
+            <SmartClassSchedulerButton />
           </div>
-          {/* <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-5 w-5 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                ></path>
-              </svg>
-            </button>
-          </div> */}
         </div>
         {children}
-        {/* <footer className="bg-secondary text-neutral font-main h-12 flex justify-left items-center pl-3">
-          Created by: Brennan Humphrey
-        </footer> */}
+        <GoogleAnalytics GA_MEASUREMENT_ID="G-DX4DXYGB7K" />
       </body>
     </html>
   );
